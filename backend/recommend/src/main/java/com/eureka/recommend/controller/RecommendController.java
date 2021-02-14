ece3c5c4e44951039cba8b23bed3fc2c97077bc1
sumin416 <sumin416@naver.com>
2021-02-15 06:02:22 +0900

    init Recommend Server

---
+++ backend/recommend/src/main/java/com/eureka/recommend/controller/RecommendController.java
@@ -0,0 +1,14 @@
+package com.eureka.recommend.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/recommend")
+@CrossOrigin(origins = "*")
+public class RecommendController {
+
+
+}
